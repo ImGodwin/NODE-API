0c86d8cd4c4f714a00e2c5043dd9d350a00603e3
Godwin <godwinmail6@gmail.com>
2024-01-28 22:10:04 +0100

    added update, and find by add endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/productModel");
+const { error } = require("console");
 const app = express();
 
 app.use(express.json()); //for middleware
@@ -19,9 +20,48 @@ app.post("/product", async (req, res) => {
   }
 });
 
-/* app.get("/user", (req, res) => {
-  res.send(`Hello Node API users`);
-}); */
+//get all products
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//get single product
+
+app.get("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//update a product
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    //if item is not inside the DB
+
+    if (!product) {
+      return res.status(404).json({ message: `cannot find any product with ID ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//delete a product
 
 mongoose.set("strictQuery", false);
 mongoose
